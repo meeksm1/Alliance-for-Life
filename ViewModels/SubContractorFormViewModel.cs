4539953f08e8baa3b35c24dd28905f51f8546bab
meeksm1 <40359876+meeksm1@users.noreply.github.com>
2019-02-05 10:53:32 -0600

    Latest Changes

--- ViewModels/SubContractorFormViewModel.cs
+++ ViewModels/SubContractorFormViewModel.cs
@@ -17,7 +17,7 @@ namespace Alliance_for_Life.ViewModels
         [Display(Name = "Organization Name")]
         public string OrgName { get; set; }
 
-        [Display(Name = "Center Director")]
+        [Display(Name = "Key Contact")]
         [Required]
         public string Director { get; set; }
 
