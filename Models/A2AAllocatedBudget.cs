e5164bdc257b55386cc258e97a7ae55055ecbdc7
meeksm1 <40359876+meeksm1@users.noreply.github.com>
2019-06-18 13:11:24 -0500

    Allocated Budget Table

---
+++ Models/A2AAllocatedBudget.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alliance_for_Life.Models
+{
+    public class A2AAllocatedBudget
+    {
+        public Guid A2AAllocatedBudgetId { get; set; }
+        public int Year { get; set; }
+        public double BeginingBalance { get; set; }
+        public double StateDeposits { get; set; }
+
+
+        public Months Month { get; set; }
+
+
+    }
+}
