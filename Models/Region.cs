38b6c10484eda3d6342c7a8a248c9b157ee0719b
meeksm1 <40359876+meeksm1@users.noreply.github.com>
2019-06-25 13:23:49 -0500

    Sub Changes

--- Models/Region.cs
+++ Models/Region.cs
@@ -7,23 +7,23 @@ namespace Alliance_for_Life.Models
 {
     public enum GeoRegion
     {
-        [Display(Name = "Region 1")]
+        [Display(Name = "Region: 1")]
         Region1  = 1,
-        [Display(Name = "Region 2")]
+        [Display(Name = "Region: 2")]
         Region2 = 2,
-        [Display(Name = "Region 3")]
+        [Display(Name = "Region: 3")]
         Region3 = 3,
-        [Display(Name = "Region 4")]
+        [Display(Name = "Region: 4")]
         Region4 = 4,
-        [Display(Name = "Region 5")]
+        [Display(Name = "Region: 5")]
         Region5 = 5,
-        [Display(Name = "Region 6")]
+        [Display(Name = "Region: 6")]
         Region6 = 6,
-        [Display(Name = "Region 7")]
+        [Display(Name = "Region: 7")]
         Region7 = 7,
-        [Display(Name = "Region 8")]
+        [Display(Name = "Region: 8")]
         Region8 = 8,
-        [Display(Name = "Region 9")]
+        [Display(Name = "Region: 9")]
         Region9 = 9 
     }
 }
