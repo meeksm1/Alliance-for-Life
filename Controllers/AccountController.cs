53a9fd669c36bd07482d88a5b99fc2d5a0a0709b
meeksm1 <40359876+meeksm1@users.noreply.github.com>
2019-07-23 15:11:58 -0500

    Set session timeout to 45 min

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -614,6 +614,7 @@ namespace Alliance_for_Life.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            HttpContext.Session.Timeout = 45;
             return RedirectToAction("Index", "Account");
         }
 
