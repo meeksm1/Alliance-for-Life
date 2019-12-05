6b1bb24cdf0b4b8a0d382a1dbab826c595c1c9f9
Manoj Ghimire <46035799+manojpsdghimire@users.noreply.github.com>
2019-12-05 10:24:55 -0600

    updates

--- ViewModels/MonthlyServices.cs
+++ ViewModels/MonthlyServices.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
+
 namespace Alliance_for_Life.ViewModels
 {
+
     public class MonthlyServices
     {
         public IEnumerable<ResidentialMIR> Residential { get; set; }
         public IEnumerable<NonResidentialMIR> NonResidential { get; set; }
+
+      
     }
+
+
+
+   
 }
